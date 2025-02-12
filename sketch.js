4ed6d79a08208f2cf1af8e489576f120fbe7b8d6
ana-konzen <143814540+ana-konzen@users.noreply.github.com>
2025-02-11 23:01:23 -0500

    update

--- sketch.js
+++ sketch.js
@@ -1,37 +1,41 @@
-let shared, me, guests;
+let shared, me;
 
 let wordTable;
 
+const writingTime = 10;
+
 function preload() {
   partyConnect("wss://demoserver.p5party.org", "ana_dict");
-  shared = partyLoadShared("globals", { word: "", status: "start", order: 0, timer: 0 });
+  shared = partyLoadShared("globals");
   me = partyLoadMyShared({ letter: "" });
-  guests = partyLoadGuestShareds();
 
   wordTable = loadTable("dict.csv", "csv", "header");
 }
 
 function setup() {
   noCanvas();
-  partySetShared(shared, { word: "", status: "start", order: 0, timer: 0 });
-  shared.players = {
-    player1: {
-      points: 0,
-      rounds: 0,
+  partySetShared(shared, {
+    word: "",
+    status: "start",
+    order: 0,
+    messageTimer: 0,
+    playTimer: writingTime,
+    turn: "player1",
+    players: {
+      player1: {
+        points: 0,
+        rounds: 0,
+        name: "Player 1",
+      },
+      player2: {
+        points: 0,
+        rounds: 0,
+        name: "Player 2",
+      },
     },
-    player2: {
-      points: 0,
-      rounds: 0,
-    },
-  };
+  });
 
-  if (partyIsHost()) {
-    me.turn = true;
-    me.key = "player1";
-  } else {
-    me.turn = false;
-    me.key = "player2";
-  }
+  me.key = partyIsHost() ? "player1" : "player2";
 
   select("#start").mousePressed(startGame);
   select("#inst-start").mousePressed(startGame);
@@ -43,30 +47,34 @@ function setup() {
 }
 
 function draw() {
-  let currentPlayer;
-  let previousPlayer;
+  const currentPlayer = shared.turn;
+  const previousPlayer = shared.turn === "player1" ? "player2" : "player1";
 
-  guests.forEach((guest) => {
-    if (guest.turn) {
-      currentPlayer = guest;
-    } else {
-      previousPlayer = guest;
+  if (partyIsHost() && shared.status === "playing") {
+    if (frameCount % 60 === 0 && shared.playTimer > 0) {
+      shared.playTimer--;
     }
-  });
+    if (shared.playTimer === 0) {
+      goToNextTurn();
+    }
+  }
 
   if (shared.status === "continue") {
-    shared.timer++;
-    if (shared.timer > 100) {
-      shared.status = "playing";
-      shared.timer = 0;
+    if (partyIsHost()) {
+      shared.messageTimer++;
+      if (shared.messageTimer > 100) {
+        shared.status = "playing";
+        shared.messageTimer = 0;
+      }
     }
   }
 
   select("#word").html(shared.word + me.letter);
+  select("#timer").html(shared.playTimer);
 
   const player = shared.status === "playing" ? currentPlayer : previousPlayer;
 
-  const statusMessage = writeMessage(player, shared.word);
+  const statusMessage = writeMessage(shared.players[player].name, shared.word);
   select("#status").class(shared.status).html(statusMessage);
 
   if (shared.status === "win" || shared.status === "lose") {
@@ -77,17 +85,9 @@ function draw() {
 }
 
 function keyPressed() {
-  if (me.turn && shared.status === "playing") {
+  if (shared.turn === me.key && shared.status === "playing") {
     if (key === "Enter") {
-      shared.word += me.letter;
-      me.turn = false;
-      me.letter = "";
-      shared.order++;
-      guests[shared.order % guests.length].turn = true;
-      if (shared.word.length > 1) {
-        shared.status = "checking";
-        shared.status = checkWord(shared.word);
-      }
+      goToNextTurn();
     }
 
     if (/^[a-zA-Z]$/.test(key)) {
@@ -124,9 +124,8 @@ function checkWord(str) {
 
 function writeMessage(player, word) {
   if (shared.status === "playing") {
-    const turnMessage = me.turn
-      ? "It's your turn! <br> Press enter to finish."
-      : `Player ${guests.indexOf(player) + 1} is writing...`;
+    const turnMessage =
+      me.key === shared.turn ? "It's your turn! <br> Press enter to finish." : `${player} is writing...`;
     return turnMessage;
   }
   if (shared.status === "checking") {
@@ -136,10 +135,10 @@ function writeMessage(player, word) {
     return `There are still words that can be formed with ${word}. Keep going!`;
   }
   if (shared.status === "lose") {
-    return `There are no words that can be formed with ${word}. Player ${guests.indexOf(player) + 1} loses!`;
+    return `There are no words that can be formed with ${word}. ${player} loses!`;
   }
   if (shared.status === "win") {
-    return `${word} is a word! Player ${guests.indexOf(player) + 1} wins!`;
+    return `${word} is a word! ${player} wins!`;
   }
 }
 
@@ -151,27 +150,40 @@ function updateScores() {
 }
 
 function goToNextRound() {
-  shared.word = "";
+  if (partyIsHost()) {
+    shared.word = "";
+    me.letter = "";
+    shared.status = "playing";
+  }
+}
+
+function goToNextTurn() {
+  shared.word += me.letter;
   me.letter = "";
-  shared.status = "playing";
+  shared.turn = partyIsHost() ? "player2" : "player1";
+  if (shared.word.length > 1) {
+    shared.status = "checking";
+    shared.status = checkWord(shared.word);
+  }
+  shared.playTimer = writingTime;
 }
 
 function startGame() {
-  shared.status = "playing";
+  if (partyIsHost()) shared.status = "playing";
   select("#instructionsScreen").style("display", "none");
   select("#startScreen").style("display", "none");
   select("#gameScreen").style("display", "block");
 }
 
 function goToStart() {
-  shared.status = "start";
+  if (partyIsHost()) shared.status = "start";
   select("#instructionsScreen").style("display", "none");
   select("#gameScreen").style("display", "none");
   select("#startScreen").style("display", "block");
 }
 
 function goToInstructions() {
-  shared.status = "start";
+  if (partyIsHost()) shared.status = "start";
   select("#instructionsScreen").style("display", "block");
   select("#gameScreen").style("display", "none");
   select("#startScreen").style("display", "none");
